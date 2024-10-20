cb8a9523bf1a5ca150febbbd044cdbb24117754e
feuvpi <fredvpg@gmail.com>
2024-10-20 19:16:25 -0300

    update la module

--- src/LinearSolver.hs
+++ src/LinearSolver.hs
@@ -1,25 +1,55 @@
-module LinearSolver (gaussianElimination) where
+module LinearSolver (gaussianEliminationSparse) where
 
-import Data.Sparse.Types
-import Data.Sparse.Matrix
-import qualified Data.Sparse.Vector as SV
-import qualified Data.Vector as V
+import SparseMatrix (SparseMatrix, getElem, updateSparse)
 
 -- Perform Gaussian elimination on a sparse matrix
-gaussianElimination :: (Num a) => SpMatrix a -> SV.SparseVector a -> Maybe (SV.SparseVector a)
-gaussianElimination mat b = backSubstitute (reduce (mat, b))
+gaussianEliminationSparse :: SparseMatrix -> [Double] -> Maybe [Double]
+gaussianEliminationSparse mat b = do
+    let n = length b
+    (mat', b') <- reduce mat b n 0
+    backSubstitute mat' b' n
 
 -- Reduce the sparse matrix to upper triangular form
-reduce :: (Num a) => (SpMatrix a, SV.SparseVector a) -> Maybe (SpMatrix a, SV.SparseVector a)
-reduce (mat, b) = case leadCol of
-  Nothing -> Nothing
-  Just col -> Just (mat', b')
+reduce :: SparseMatrix -> [Double] -> Int -> Int -> Maybe (SparseMatrix, [Double])
+reduce mat b n k
+  | k >= n = Just (mat, b)
+  | otherwise = 
+      case pivot of
+        0 -> Nothing  -- Cannot proceed if pivot is zero
+        _ -> reduce mat'' b'' n (k + 1)
   where
-    leadCol = V.findIndex (== maximum (V.map abs (getNonZeroValues mat))) (getNonZeroValues mat)
-    mat' = mapRows (\i row -> if i == col then row else row - (getElem mat i col) * (getRow mat col) / (getElem mat col col)) mat
-    b' = SV.mapWithIndex (\i val -> val - (getElem mat i col) * (SV.getElem b col) / (getElem mat col col)) b
+    pivot = getElem mat k k
+    eliminateRow (matAcc, bAcc) i =
+      let factor = getElem matAcc i k / pivot
+          matAcc' = foldl (\m j ->
+                      let a_ij = getElem m i j
+                          a_kj = getElem m k j
+                          value = a_ij - factor * a_kj
+                      in if value /= 0
+                         then updateSparse m i j value
+                         else m
+                    ) matAcc [k..n-1]
+          b_i = bAcc !! i
+          b_k = bAcc !! k
+          b_i' = b_i - factor * b_k
+          bAcc' = replaceNth i b_i' bAcc
+      in (matAcc', bAcc')
+    (mat'', b'') = foldl eliminateRow (mat, b) [k + 1 .. n - 1]
 
 -- Back substitution
-backSubstitute :: (Num a) => Maybe (SpMatrix a, SV.SparseVector a) -> Maybe (SV.SparseVector a)
-backSubstitute Nothing = Nothing
-backSubstitute (Just (mat, b)) = Just (SV.generate (SV.length b) (\i -> (SV.getElem b i - sum (zipWith (*) (map (\j -> getElem mat i j) [0..i-1]) (map (\j -> SV.getElem b j) [0..i-1]))) / (getElem mat i i)))
+backSubstitute :: SparseMatrix -> [Double] -> Int -> Maybe [Double]
+backSubstitute mat b n = backSubstitute' (n - 1) []
+  where
+    backSubstitute' (-1) xs = Just xs
+    backSubstitute' i xs =
+      let sum_ax = sum [ getElem mat i j * xj | (j, xj) <- zip [i+1..n-1] xs ]
+          xi = (b !! i - sum_ax) / getElem mat i i
+      in if getElem mat i i == 0
+          then Nothing  -- Cannot divide by zero
+          else backSubstitute' (i - 1) (xi : xs)
+
+-- Helper function to replace an element in a list
+replaceNth :: Int -> a -> [a] -> [a]
+replaceNth _ _ [] = []
+replaceNth 0 x (_:xs) = x : xs
+replaceNth i x (y:ys) = y : replaceNth (i - 1) x ys
--- src/SparseMatrix.hs
+++ src/SparseMatrix.hs
@@ -1,13 +1,23 @@
-module SparseMatrix {
-    SparseMatrix,
-    lookupSparse,
-    updateSparse
-} where
+module SparseMatrix (SparseMatrix, getElem, lookupSparse, updateSparse) where
 
--- Type definition for sparse matrix
-type SparseMatrix = [(Int, Int, Double)]
+-- Define a data structure to represent a sparse matrix
+newtype SparseMatrix = SparseMatrix [(Int, Int, Double)] deriving (Show, Eq) -- (row, column, value)
+
+-- Function to access an element of the sparse matrix
+getElem :: SparseMatrix -> Int -> Int -> Double
+getElem (SparseMatrix elems) i j = 
+    case lookup (i, j) (map (\(r, c, v) -> ((r, c), v)) elems) of
+        Just value -> value
+        Nothing -> 0.0 -- If there's no defined value, return 0
 
 -- Function to lookup a value in the sparse matrix
 lookupSparse :: SparseMatrix -> Int -> Int -> Double
-lookupSparse [] _ _ = 0
-lookupSparse ()
+lookupSparse (SparseMatrix []) _ _ = 0 -- If the matrix is empty, return 0
+lookupSparse (SparseMatrix ((r, c, v):xs)) row col
+    | r == row && c == col = v -- If the row and column match, return the value
+    | otherwise = lookupSparse (SparseMatrix xs) row col -- Continue searching in the rest of the matrix
+
+-- Function to update a value in a sparse matrix
+updateSparse :: SparseMatrix -> Int -> Int -> Double -> SparseMatrix
+updateSparse (SparseMatrix elems) i j value =
+    SparseMatrix (filter (\(x, y, _) -> x /= i || y /= j) elems ++ [(i, j, value)])
